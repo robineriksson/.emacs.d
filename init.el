a4aa3f62d31c177774ea3c3b4d6c9ea266349e38
robineriksson <robineriksson@gmail.com>
2017-07-03 17:30:56 +0200

    did some work on the dotfile

--- init.el
+++ init.el
@@ -1,14 +1,64 @@
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; hide toolbar
+;;
+;; https://www.emacswiki.org/emacs/ToolBar#toc1
+(tool-bar-mode -1)
+
+;; hide scrollbar
+;;
+;; https://www.emacswiki.org/emacs/ScrollBar
+(scroll-bar-mode -1)
+
+;; hide menubar
+;;
+;; https://www.emacswiki.org/emacs/MenuBar
+(menu-bar-mode -1)
+
+;; hide splash screen
+;;
+;; http://stackoverflow.com/a/744681/3034693
+(setq inhibit-startup-screen t)
+
+;; hide scratch message
+;;
+;; https://stackoverflow.com/questions/1498258/how-do-i-change-the-scratch-message-in-emacs
+(setq initial-scratch-message nil)
+
+
+
+;; custom theme, for looks.
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
-(add-to-list 'load-path "/home/robin/Gits/ESS/lisp/")
-(require 'ess-site)
+;;(require 'dracula-theme')
+;;(set-frame-font "Inconsolata-14")
+;; enable ess (R interpretor)
+(use-package ess-site
+  :load-path "/home/robin/Gits/ESS/lisp/"
+    :commands R)
+;;(add-to-list 'load-path "/home/robin/Gits/ESS/lisp/")
+;;(require 'ess-site)
+;;tilde and other symbols
 (require 'iso-transl)
 
-(setq smerge-command-prefix "\C-cv")
-(defun my-enable-smerge-maybe ()
-  (when (and buffer-file-name (vc-backend buffer-file-name))
-    (save-excursion
-      (goto-char (point-min))
-      (when (re-search-forward "^<<<<<<< " nil t)
-	(smerge-mode +1)))))
-(add-hook 'buffer-list-update-hook #'my-enable-smerge-maybe)
+;; add melpa package list
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; flycheck (syntax checking)
+(use-package flycheck
+	     :ensure t
+	       :init (global-flycheck-mode))
+
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
